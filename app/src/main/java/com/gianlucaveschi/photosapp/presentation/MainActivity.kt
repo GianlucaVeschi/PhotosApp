65bea38aa30cb63b982c9817eec58b80e77e37e7
Gianluca Veschi <sheckleeer@gmail.com>
2023-01-26 19:17:28 +0100

    Simplify MainActivity

--- app/src/main/java/com/gianlucaveschi/photosapp/presentation/MainActivity.kt
+++ app/src/main/java/com/gianlucaveschi/photosapp/presentation/MainActivity.kt
@@ -1,14 +1,8 @@
 package com.gianlucaveschi.photosapp.presentation
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.gianlucaveschi.photosapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.main_activity)
